b9ddd0c839aa2f9b0c07d4f64af31ba21dba386d
bingnuoer <2366858264@qq.com>
2023-12-01 09:16:08 +0800

    修改头像

--- 05.案例-个人信息设置/js/index.js
+++ 05.案例-个人信息设置/js/index.js
@@ -3,12 +3,13 @@
  *  1.1 获取用户的数据
  *  1.2 回显数据到标签上
  * */
+const creator = '播仔'
 // 1.1 获取用户的数据
 axios({
     url:'http://hmajax.itheima.net/api/settings',
     method:'GET',
     params:{
-        creator:'波仔'
+        creator
     }
 }).then(result => {
     console.log(result.data.data);
@@ -36,3 +37,33 @@ axios({
 
     })
 })
+
+/**
+ * 目标2：修改头像
+ *  1.1 获取用户选择的头像信息
+ *  1.2 发送请求获取头像url(图片文件转换成form文件)
+ *  1.3 获取头像标签渲染url
+ * */
+// 1.1 获取用户的头像信息
+// lable标签扩大选择器范围
+document.querySelector('.upload').addEventListener('change',e => {
+    console.log(e.target.files[0]);
+    // 图片文件转换成form文件
+    const fd = new FormData()
+    // 请求携带的2个参数，添加到form文件中
+    fd.append('avatar',e.target.files[0])
+    fd.append('creator',creator)
+    // 1.2 发送请求获取头像url(图片文件转换成form文件)
+    axios({
+        url:'http://hmajax.itheima.net/api/avatar',
+        method:'PUT',
+        data:fd
+    }).then(result => {
+        // console.log(result);
+        // 1.3 获取头像标签渲染url
+        const imgUrl = result.data.data.avatar
+        document.querySelector('.prew').src = imgUrl
+    })
+
+})
+
